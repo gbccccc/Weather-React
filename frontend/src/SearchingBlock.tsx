4d12971b50e8ae4fd7205267aad5d9df844a7425
gbccccc <gbccccc233@gmail.com>
2024-11-08 23:45:06 -0800

    Header

---
+++ frontend/src/SearchingBlock.tsx
@@ -0,0 +1,11 @@
+import "styles/SearchingBlock.css"
+
+function SearchingBlock() {
+  return (
+      <div>
+        <h2> Weather Search🌥️ </h2>
+      </div>
+  )
+}
+
+export default SearchingBlock;
