aead78016c36cc0ada46774f5c89eca297974ee2
gbccccc <gbccccc233@gmail.com>
2024-11-10 11:38:05 -0800

    disable form when checking autodetect

--- frontend/src/SearchingBlock.tsx
+++ frontend/src/SearchingBlock.tsx
@@ -3,7 +3,7 @@ import stateMappingJson from "assets/jsons/state-mapping.json"
 import {Button, Col, Form, Row} from "react-bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useRef} from "react";
+import React, {useRef, useState} from "react";
 
 function SearchingBlock({submitCallback, clearCallback}: {
   submitCallback: (needAutodetect: boolean, formData: string) => void,
@@ -11,6 +11,7 @@ function SearchingBlock({submitCallback, clearCallback}: {
 }) {
   let formRef = useRef<HTMLFormElement>(null)
   let autodetectRef = useRef<HTMLInputElement>(null)
+  let [disabled, setDisabled] = useState(false)
 
   const stateOptions = Object.entries(stateMappingJson).map(
       ([key, value]) => <option value={key} key={key}>{value}</option>
@@ -27,6 +28,11 @@ function SearchingBlock({submitCallback, clearCallback}: {
     submitCallback(autodetectRef.current!["checked"], addressStr)
   }
 
+  function onAutodetectChange(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(disabled)
+    setDisabled(e.target.checked)
+  }
+
   return (
       <div className="searching-block">
         <h2> Weather Search🌥️ </h2>
@@ -35,36 +41,39 @@ function SearchingBlock({submitCallback, clearCallback}: {
             <Col sm={2}/>
             <Form.Label className="address-label" column sm={1}>Street</Form.Label>
             <Col sm={6}>
-              <Form.Control className="form-input" type="input" required name="street"></Form.Control>
+              <Form.Control className="form-input" type="input" required name="street" disabled={disabled}></Form.Control>
             </Col>
           </Form.Group>
           <Form.Group as={Row} className="mb-3" controlId="city">
             <Col sm={2}/>
             <Form.Label className="address-label" column sm={1}>City</Form.Label>
             <Col sm={6}>
-              <Form.Control className="form-input" type="input" required name="city"></Form.Control>
+              <Form.Control className="form-input" type="input" required name="city" disabled={disabled}></Form.Control>
             </Col>
           </Form.Group>
           <Form.Group as={Row} className="mb-3" controlId="state">
             <Col sm={2}/>
             <Form.Label className="address-label" column sm={1}>State</Form.Label>
             <Col sm={3}>
-              <Form.Select required name="state">
+              <Form.Select required name="state" disabled={disabled}>
                 <option value="" key="holder">Select Your State</option>
                 {stateOptions}
               </Form.Select>
             </Col>
           </Form.Group>
-          <hr/>
-          <Form.Group as={Row} controlId="autodetect" className="checkbox-row">
-            <Form.Label column xs="auto">Autodetect Location</Form.Label>
-            <Col xs="auto" className="checkbox-row">
-              <Form.Check name="autodetect" ref={autodetectRef} label="Current Location"></Form.Check>
-            </Col>
-          </Form.Group>
-          <Button className="form-button" variant="primary" onClick={onSubmit}><i className="bi bi-search"></i>Search</Button>
-          <Button className="form-button" variant="outline-secondary"><i className="bi bi-list-nested"></i>Clear</Button>
         </Form>
+        <hr/>
+        <Form.Group as={Row} controlId="autodetect" className="checkbox-row">
+          <Form.Label column xs="auto">Autodetect Location</Form.Label>
+          <Col xs="auto" className="checkbox-row">
+            <Form.Check name="autodetect" ref={autodetectRef} onChange={onAutodetectChange}
+                        label="Current Location"></Form.Check>
+          </Col>
+        </Form.Group>
+        <Button className="form-button" variant="primary" onClick={onSubmit}><i
+            className="bi bi-search"></i>Search</Button>
+        <Button className="form-button" variant="outline-secondary"><i
+            className="bi bi-list-nested"></i>Clear</Button>
       </div>
   )
 }
