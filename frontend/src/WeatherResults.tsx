ccd61f52939e5bbf990ea14cdc0a5dfa637d99f5
gbccccc <gbccccc233@gmail.com>
2024-11-10 17:04:51 -0800

    results table bugs fixed

--- frontend/src/WeatherResults.tsx
+++ frontend/src/WeatherResults.tsx
@@ -1,6 +1,7 @@
 import Table from 'react-bootstrap/Table';
 import {WeatherApiResult, WeatherStats} from "./styles/interfaces";
 import {weatherMapping} from "./mappings";
+import "./styles/WeatherResults.css"
 
 function WeatherResults({weatherStats, address}: {
   weatherStats: WeatherApiResult,
@@ -9,13 +10,13 @@ function WeatherResults({weatherStats, address}: {
   const tableRows = weatherStats.forecast.data.timelines[0].intervals.map(
       (intervalStat, index) =>
           <tr>
-            <td>{index}</td>
+            <td>{index + 1}</td>
             <td>{intervalStat.startTime}</td>
             <td>
-              {weatherMapping[intervalStat.values.weatherCode as keyof typeof weatherMapping].description}
               <img className="table-status-icon"
-                   src={`src/assets/images/weather-symbols/${weatherMapping[intervalStat.values.weatherCode as keyof typeof weatherMapping].iconName}`}
+                   src={require(`assets/images/weather-symbols/${weatherMapping[intervalStat.values.weatherCode as keyof typeof weatherMapping].iconName}`)}
                    alt={weatherMapping[intervalStat.values.weatherCode as keyof typeof weatherMapping].iconName}/>
+              {weatherMapping[intervalStat.values.weatherCode as keyof typeof weatherMapping].description}
             </td>
             <td>{intervalStat.values.temperatureMax}</td>
             <td>{intervalStat.values.temperatureMax}</td>
@@ -24,21 +25,23 @@ function WeatherResults({weatherStats, address}: {
   )
 
   return (
-      <Table striped bordered hover className="mt-3">
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>Date</th>
-          <th>Status</th>
-          <th>Temp. High(&deg;F)</th>
-          <th>Temp. Low(&deg;F)</th>
-          <th>Wind Speed(mph)</th>
-        </tr>
-        </thead>
-        <tbody>
-        {tableRows}
-        </tbody>
-      </Table>
+      <div className="weather-results">
+        <Table striped bordered hover className="mt-3 weather-results-table">
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>Date</th>
+            <th>Status</th>
+            <th>Temp. High(&deg;F)</th>
+            <th>Temp. Low(&deg;F)</th>
+            <th>Wind Speed(mph)</th>
+          </tr>
+          </thead>
+          <tbody>
+          {tableRows}
+          </tbody>
+        </Table>
+      </div>
   );
 }
 
